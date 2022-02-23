876ada5ab93918fd770d7093c3af8727032a9ba2
Indramohan <indra.pillai@gmail.com>
2022-02-23 20:23:07 +0530

    Comments in bash
    single line and multi line comments

---
+++ 4 comments.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+
+echo "single line comment"
+# is used like this
+
+echo "multi-line comments"
+
+: '
+is used like this
+is used like this
+is used like this
+is used like this
+is used like this
+is used like this
+v
+is used like this
+is used like this
+is used like this
+is used like this
+'
